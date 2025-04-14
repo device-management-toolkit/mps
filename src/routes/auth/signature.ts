7cbc41e66468625802801e02b0bc2f07e28d5c65
Mike <michael.johanson@intel.com>
2025-04-14 10:08:04 -0700

    fix: grab req.tenantId for device token
    addresses issue #1767

--- src/routes/auth/signature.ts
+++ src/routes/auth/signature.ts
@@ -6,12 +6,12 @@
 import { Environment } from '../../utils/Environment.js'
 import jws from 'jws'
 
-export function signature(expirationMinutes, device: string): any {
+export function signature(expirationMinutes, device, tenantId: string): any {
   const expiration = Math.floor((Date.now() + 1000 * 60 * expirationMinutes) / 1000)
   const signature = jws.sign({
     header: { alg: 'HS256', typ: 'JWT' },
     payload: {
-      tenantId: '',
+      tenantId,
       iss: Environment.Config.jwt_issuer,
       deviceId: device,
       exp: expiration
